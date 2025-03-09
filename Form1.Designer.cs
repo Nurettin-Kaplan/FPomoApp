3f5400398ec3d2b4f1516bf3a2d1acec606bb28c
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2025-03-09 16:45:05 +0300

    The form window and buttons haave been coded.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FPomoApp
 {
-    partial class Form1
+    partial class App
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(App));
+            panel1 = new Panel();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            BtnClose = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(173, 176, 176);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(BtnClose);
+            panel1.Dock = DockStyle.Top;
+            panel1.ForeColor = SystemColors.GrayText;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1493, 44);
+            panel1.TabIndex = 0;
+            panel1.MouseDown += panel1_MouseDown;
+            panel1.MouseMove += panel1_MouseMove;
+            panel1.MouseUp += panel1_MouseUp;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.FromArgb(150, 26, 37);
+            label1.Location = new Point(50, 10);
+            label1.Name = "label1";
+            label1.Size = new Size(77, 25);
+            label1.TabIndex = 3;
+            label1.Text = "FPOMO";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Left;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(53, 44);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(173, 176, 176);
+            button1.Dock = DockStyle.Right;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Adobe Gothic Std B", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(1404, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(42, 44);
+            button1.TabIndex = 1;
+            button1.Text = "_";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // BtnClose
+            // 
+            BtnClose.BackColor = Color.FromArgb(150, 26, 37);
+            BtnClose.Dock = DockStyle.Right;
+            BtnClose.FlatAppearance.BorderSize = 0;
+            BtnClose.FlatStyle = FlatStyle.Flat;
+            BtnClose.ForeColor = Color.White;
+            BtnClose.Location = new Point(1446, 0);
+            BtnClose.Name = "BtnClose";
+            BtnClose.Size = new Size(47, 44);
+            BtnClose.TabIndex = 0;
+            BtnClose.Text = "X";
+            BtnClose.UseVisualStyleBackColor = false;
+            BtnClose.Click += BtnClose_Click;
+            // 
+            // App
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(173, 176, 176);
+            ClientSize = new Size(1493, 736);
+            Controls.Add(panel1);
+            Font = new Font("Yu Gothic UI Semibold", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4);
+            Name = "App";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "FPOMO";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Button button1;
+        private Button BtnClose;
+        private PictureBox pictureBox1;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,46 @@
 namespace FPomoApp
 {
-    public partial class Form1 : Form
+    public partial class App : Form
     {
-        public Form1()
+        public App()
         {
             InitializeComponent();
         }
+
+        private Point _mouseDownLocation;
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _mouseDownLocation = e.Location; // Fare týklama konumunu kaydet
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                // Fare hareketi ile formu taþý
+                this.Location = new Point(
+                    this.Left + (e.X - _mouseDownLocation.X),
+                    this.Top + (e.Y - _mouseDownLocation.Y)
+                );
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            // Sürüklemeyi sonlandýrmak için herhangi bir þey yapmamýza gerek yok
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); // Uygulamanýn kapanmasýný saðlar
+        }
+
+
     }
 }
